125cfcf462121b4837d43c5e88c1b471816c790e
edyfz1132 <52634384+edyfz1132@users.noreply.github.com>
2019-07-24 19:25:40 +0300

    Update botLogin.js

--- config/botLogin.js
+++ config/botLogin.js
@@ -1,5 +1,4 @@
 module.exports = {
-	token: "TOKEN_KEY",
-	youtubeAPI: "YOUTUBE_API_KEY",
-	owner_id: "OWNER_ID"
-}
+	token: "NTI0Mjk2NDA0Nzg1NTYxNjE2.XTiF0Q.GyGZjanzzEJuUmC3X30AHEss-2c",
+	youtubeAPI: "NTkMTQzMTI1NTY0NzE5MTMz.XRJIvghE6-2Jcb0hewGsxXu6AAZUqWAIw",
+	owner_id: "458174267692154882"
